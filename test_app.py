86478e2f59db08dd6aa3d266fc60f36795850d4d
chinedunewbirth <chineduchukwuemekamazi@gmail.com>
2025-01-10 11:30:48 +0000

    updated py_test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 from app import app
 
 def test_hello_world():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b'Hello, World!'
